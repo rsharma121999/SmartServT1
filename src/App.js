3324a7f2b7f4365e203d0fd8d653f1e3a5abce66
Rishabh Sharma <r.sharma121999@gmail.com>
2021-12-30 02:34:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [productsList, setProductsList] = useState([]);
+
+  const sortList = arr => {
+    arr.sort(function (a, b) {
+      var x = Number(a['popularity']);
+      var y = Number(b['popularity']);
+      return x < y ? 1 : x > y ? -1 : 0;
+    });
+  };
+
+  useEffect(() => {
+    fetch(`https://s3.amazonaws.com/open-to-cors/assignment.json`, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(response => {
+        let prods = [];
+        let products = response.products;
+        prods = Object.entries(products).map(e => e[1]);
+        setProductsList(prods);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(() => {});
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {productsList && sortList(productsList)}
+      {productsList.map((prod, key) => {
+        return (
+          <div className="card" key={key}>
+            <div className="box1">
+              <h3>Title</h3>
+              <h3>{prod.title}</h3>
+            </div>
+            <div className="box2">
+              <h3>Price: </h3>
+              <h3>{prod.price}</h3>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
